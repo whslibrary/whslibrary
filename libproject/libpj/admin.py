93384e47dd447063bb40b0c92031bfa9ccc5439d
whslibrary <159443177+whslibrary@users.noreply.github.com>
2024-02-08 12:28:43 +0000

    Update admin.py

--- libproject/libpj/admin.py
+++ libproject/libpj/admin.py
@@ -3,5 +3,5 @@ from .models import Profile, Pwdlist, Book
 
 # Register your models here.
 admin.site.register(Profile)
-admin.site.register(Pwdlist)
+#admin.site.register(Pwdlist)
 admin.site.register(Book)
